b8bd5241ea0409832c8b7383debea5776a72cd2d
xiong ding <xiong.ding@airbnb.com>
2019-02-20 15:01:55 -0800

    ignore

--- linux_setup.sh
+++ linux_setup.sh
@@ -99,7 +99,7 @@ alias la='ls -A'
 # main editor
 export EDITOR="vim"
 # set the bash prompts
-export PS1="\e[0;36m$PS1\e[m"
+export PS1="\e[0;35m$PS1\e[m"
 # PS1='\[\e[0;44m\]$NAME\[\e[m\] \[\e[0;31m\]\u\[\e[m\] \[\e[0;36m\]\w\[\e[m\] \[\e[1;32m\]\$\[\e[m\] \[\e[0;37m\]'
 alias vbr='vim ~/.bashrc'
 alias sbr='source ~/.bashrc'
